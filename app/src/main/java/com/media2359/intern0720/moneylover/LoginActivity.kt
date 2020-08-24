763270c6a7ade153b867fcd216a3e6f78385208c
Kien <kienlun0903@gmail.com>
2020-08-24 13:53:50 +0700

    Update Ui Login screen

---
+++ app/src/main/java/com/media2359/intern0720/moneylover/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.media2359.intern0720.moneylover
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
